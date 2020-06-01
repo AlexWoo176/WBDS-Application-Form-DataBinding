808737edecee7c2b014c4b9e0b010e760e956ef3
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-06-01 23:11:50 +0700

    Fix

--- src/main/java/com/codegym/controller/EmployeeController.java
+++ src/main/java/com/codegym/controller/EmployeeController.java
@@ -4,20 +4,20 @@ import com.codegym.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/")
+//@RequestMapping("/")
 public class EmployeeController {
-    @RequestMapping(value = "showform", method = RequestMethod.GET)
+//    @RequestMapping(value = "showform", method = RequestMethod.GET)
+    @GetMapping("/")
     public String showForm(ModelMap model) {
         model.addAttribute("employee", new Employee());
         return "create";
     }
 
-    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+//    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    @PostMapping("/addEmployee")
     public String submit(@ModelAttribute("emlpoyee") Employee employee, BindingResult result, ModelMap model) {
         model.addAttribute("name", employee.getName());
         model.addAttribute("contactNumber", employee.getContactNumber());
